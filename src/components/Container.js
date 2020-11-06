4382d5d1a2b92b5b71a55bc8785e9af28ac5d599
hilbug <65197724+hilbug@users.noreply.github.com>
2020-11-05 19:18:55 -0500

    remove unncessary files

--- src/components/Container.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Container(props) {
-  return <div className={`container${props.fluid ? "-fluid" : ""}`}>{props.children}</div>;
-}
-
-export default Container;
-
-// Week 19 - Act 20
