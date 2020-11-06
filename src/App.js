fc648ced38750debc9a66241f2d947b0a6a50eda
hilbug <65197724+hilbug@users.noreply.github.com>
2020-11-05 21:58:42 -0500

    add resume and footer to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Header from "./components/Header";
-import Portfolio from "./pages/Portfolio"
+import Portfolio from "./pages/Portfolio";
+import Footer from "./components/Footer";
+import Resume from "./components/Resume";
 
 function App() {
   return (
     <div>
       <Header />
       <Portfolio />
+      <Resume />
+      <Footer />
     </div>
   );
 }
