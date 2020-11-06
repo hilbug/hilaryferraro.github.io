f6f381b02ce5c86083ee90ac3a5dbd6e77acd47b
hilbug <65197724+hilbug@users.noreply.github.com>
2020-11-05 21:58:19 -0500

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer class="text-white mt-0">
+        <div class="container">
+            <p class="float-right animate-link">
+                <a href="#top" class="text-reset text-decoration-none">Back to top</a>
+            </p>
+            <p>Hilary Ferraro © 2020</p>
+            <p>Made with &copy; Bootstrap</p>
+        </div>
+    </footer>
+    );
+}
+
+export default Footer;
