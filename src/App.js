3cc1bd296318b2afcd99e27be74ceac7738847ff
hilbug <65197724+hilbug@users.noreply.github.com>
2020-11-05 22:07:26 -0500

    add color shape btw resume and footer

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ function App() {
       <Header />
       <Portfolio />
       <Resume />
+      {/* SVG Image for angle */}
+      <svg class="mb-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                <path fill="#bc8f8f" fill-opacity="1" d="M0,224L1440,160L1440,320L0,320Z"></path>
+            </svg>
       <Footer />
     </div>
   );
