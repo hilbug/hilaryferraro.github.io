ade5152c21f127400d5891a2c25fa520a6141058
hilbug <65197724+hilbug@users.noreply.github.com>
2020-11-07 16:23:29 -0500

    add Introduction page

---
+++ src/pages/Introduction.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Jumbo from "../components/Jumbotron/Jumbotron";
+import Resume from "../components/Resume";
+
+const headline = "Meet Hilary";
+const leadText = "An aspiring full stack developer after 10+ years on the operations side of healthcare technology. Currently enrolled in UNH's Coding Bootcamp. Acquiring proficiency in HTML, CSS, Javascript, jQuery, Node.js, MySQL, APIs/AJAX, Express, React, Perl. Loves her Boston Terrier, reading, royal-watching, and travel shows.";
+
+const Introduction = () => {
+    return (
+        <div>
+            <Jumbo
+                headline={headline}
+                leadText={leadText}
+            />
+            <Resume />
+        </div>
+    );
+}
+
+export default Introduction;
+
+// from previous portfolio
