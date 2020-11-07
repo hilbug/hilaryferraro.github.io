682a113fa86c4bb92970856378016f7280ea0dd0
hilbug <65197724+hilbug@users.noreply.github.com>
2020-11-07 16:23:39 -0500

    add Introduction to App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
+import React from "react";
 import Header from "./components/Header";
+import Introduction from "./pages/Introduction";
 import Portfolio from "./pages/Portfolio";
 import Footer from "./components/Footer";
-import Resume from "./components/Resume";
 
 function App() {
   return (
     <div>
       <Header />
+      <Introduction />
       <Portfolio />
-      <Resume />
       {/* SVG Image for angle */}
-      <svg class="mb-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                <path fill="#bc8f8f" fill-opacity="1" d="M0,224L1440,160L1440,320L0,320Z"></path>
+      <svg className="mb-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                <path fill="#bc8f8f" fillOpacity="1" d="M0,224L1440,160L1440,320L0,320Z"></path>
             </svg>
       <Footer />
     </div>
