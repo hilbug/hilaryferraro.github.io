4a73d39e00769d687571a160c960b4a672996731
hilbug <65197724+hilbug@users.noreply.github.com>
2020-11-07 16:20:40 -0500

    update Footer className

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,10 @@ import React from "react";
 
 function Footer() {
     return (
-        <footer class="text-white mt-0">
-        <div class="container">
-            <p class="float-right animate-link">
-                <a href="#top" class="text-reset text-decoration-none">Back to top</a>
+        <footer className="text-white mt-0">
+        <div className="container">
+            <p className="float-right animate-link">
+                <a href="#top" className="text-reset text-decoration-none">Back to top</a>
             </p>
             <p>Hilary Ferraro © 2020</p>
             <p>Made with &copy; Bootstrap</p>
