61e9f3ccb67401eda118fbfa9c1e669d94e69e38
hilbug <65197724+hilbug@users.noreply.github.com>
2020-11-07 23:14:58 -0500

    remove resume from Intro page

--- src/pages/Introduction.js
+++ src/pages/Introduction.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Jumbo from "../components/Jumbotron/Jumbotron";
-import Resume from "../components/Resume";
 
 const headline = "Meet Hilary";
 const leadText = "An aspiring full stack developer after 10+ years on the operations side of healthcare technology. Currently enrolled in UNH's Coding Bootcamp. Acquiring proficiency in HTML, CSS, Javascript, jQuery, Node.js, MySQL, APIs/AJAX, Express, React, Perl. Loves her Boston Terrier, reading, royal-watching, and travel shows.";
@@ -12,7 +11,6 @@ const Introduction = () => {
                 headline={headline}
                 leadText={leadText}
             />
-            <Resume />
         </div>
     );
 }
